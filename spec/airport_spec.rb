a341c3b49798f73f49b840e1289d8b34e5104722
Katy McCann <katy.alice.mccann@gmail.com>
2017-02-22 12:43:18 +0000

    Create land method that takes plane and lands it

---
+++ spec/airport_spec.rb
@@ -0,0 +1,27 @@
+require 'airport'
+
+describe Airport do
+  let(:airport) {Airport.new}
+
+
+  describe "#initialize" do
+    it 'starts with an empty airport' do
+    expect(airport.planes).to be_empty
+  end
+  end
+  describe "#land" do
+    it { is_expected.to respond_to(:land) }
+
+    it 'will respond to land with one arguent' do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
+
+    it 'will have one plane once landed' do
+      plane = double("plane")
+      p airport.land(plane)
+      expect(airport.planes).to eq([plane])
+    end
+  end
+
+
+end
